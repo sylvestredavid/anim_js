3a42268122df145a290682562c3e75b5ac4200c9
Gosuhan <david.sylvestre.lp2@gmail.com>
2020-03-11 21:26:33 +0100

    ajout key events

--- script.js
+++ script.js
@@ -1,38 +1,109 @@
-(function () {
+let img;
+let imgProp = {
+    left: 10,
+    top: 100,
+    run1: './img/droite1.png',
+    run2: './img/droite2.png',
+}
+let carreaux = [];
+let colision = false;
+let start = false;
+let body;
 
-    let left = 0;
-    let posLeftCarreau = 200;
-    let img = document.createElement('img');
-    img.src = './img/droite1.png';
+function createImg() {
+    img = document.createElement('img');
+    img.src = imgProp.run1;
     img.style.position = 'absolute';
-    img.style.top = "100px";
-    img.style.left = left + "px";
+    img.style.top = imgProp.top + "px";
+    img.style.left = imgProp.left + "px";
+    body.appendChild(img);
+}
+
+function generateBlocs() {
+    for(let i = 200; i < window.innerWidth; i += 200) {
+        createBloc(i)
+    }
+}
+
+function createBloc(left) {
     let carreau = document.createElement('div');
-    carreau.style.height = "50px";
-    carreau.style.width = "50px";
+    carreau.style.height = "40px";
+    carreau.style.width = "20px";
     carreau.style.backgroundColor = "red";
     carreau.style.position = 'absolute';
-    carreau.style.top = "110px";
-    carreau.style.left = posLeftCarreau + "px";
-    let body = document.getElementById("body");
-    body.appendChild(img);
+    carreau.style.top = "120px";
+    carreau.style.left = left + "px";
     body.appendChild(carreau);
+    carreaux.push(carreau);
+}
 
-    setInterval(() => {
-        left += 5;
-        img.style.left = left + "px";
-        if(left >= posLeftCarreau - img.clientWidth) {
-            posLeftCarreau = left + img.clientWidth;
-            carreau.style.left = posLeftCarreau + "px";
-        }
-        if(left % 2 === 0) {
-            img.src = './img/droite1.png';
+function run() {
+    let run = setInterval(() => {
+        imgProp.left += 5;
+        img.style.left = imgProp.left + "px";
+        carreaux.forEach(carreau => {
+            console.log(carreau.offsetLeft)
+            if(((imgProp.left + img.clientWidth >= carreau.offsetLeft) && (imgProp.left <= carreau.offsetLeft + carreau.clientWidth)) && (imgProp.top + img.clientHeight >= 120)) {
+                colision = true;
+            }
+        })
+        if(imgProp.left % 2 === 0 && imgProp.top === 100) {
+            img.src = imgProp.run1;
         } else {
-            img.src = './img/droite2.png';
+            img.src = imgProp.run2;
         }
-        if(left >= window.innerWidth) {
-            left = 0;
+        if(imgProp.left >= window.innerWidth) {
+            imgProp.left = 0;
         }
-    }, 100)
+        if(colision) {
+            clearInterval(run)
+        }
+    }, 50)
+}
+
+function jump() {
+    document.addEventListener('keydown', (e) => {
+        if(e.code === "Space") {
+           let jump = setInterval(() => {
+                if(imgProp.top >= 30) {
+                    imgProp.top -= 1;
+                    img.style.top = imgProp.top + "px";
+                } else {
+                    clearInterval(jump)
+                }
+               if(colision) {
+                   clearInterval(jump)
+               }
+            }, 10);
+           setTimeout(() => {
+               let jump = setInterval(() => {
+                   img.src = imgProp.run2
+                   if(imgProp.top <= 100) {
+                       imgProp.top += 1;
+                       img.style.top = imgProp.top + "px";
+                   } else {
+                       imgProp.top = 100;
+                       img.style.top = imgProp.top + "px";
+                       clearInterval(jump)
+                   }
+                   if(colision) {
+                       clearInterval(jump)
+                   }
+               }, 10);
+           }, 1100)
+        }
+    })
+}
+
+(function () {
+    body = document.getElementById("body");
+    createImg();
+    generateBlocs();
+    document.addEventListener('keydown', (e) => {
+        if (e.code !== "Space") {
+            run()
+        }
+    })
+    jump()
 
 })();
